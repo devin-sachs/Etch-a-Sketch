794ba733668ba7d79c25c944a020fd4a37524c8b
devin-sachs <75508790+devin-sachs@users.noreply.github.com>
2024-03-07 20:08:56 -0800

    Create HTML, CSS, and JS skeleton. Add simple logic in JS for creating div using for loop

---
+++ script.js
@@ -0,0 +1,7 @@
+//Create 16x16 pattern of divs
+
+for(divCounter = 0; divcounter <= 16; divCounter++ ){ 
+    let gridDiv = document.createElement('div');
+    resultsDiv.setAttribute('class', 'grid-container')
+    document.body.appendChild(gridDiv); 
+}
