9fad5d778cd74e406eda730e7dff72711214db75
devin-sachs <75508790+devin-sachs@users.noreply.github.com>
2024-03-07 20:56:49 -0800

    Add logic in JS to generate grid of divs 16x16 Style divs with placeholder value to visualize in development Use flexbox to allow div grid to adjust in size

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 //Create 16x16 pattern of divs
+function createDivs(){ 
+    //divCounter = 0;
+    //for(divCounterY = 0; divCounterY <= 16; divCounterY++ ){ 
+        // const rowDiv = document.createElement('div');
+        // rowDiv.setAttribute('class', 'grid-container')
+        // rowDiv.textContent = 'a';
+        // document.body.appendChild(rowDiv); 
+        
+        // for(divCounterX =0; divCounterX <=16; divCounterX++){ 
+        //     const colDiv = document.createElement('div');
+        //     colDiv.setAttribute('class', 'grid-container')
+        //     colDiv.textContent = 'b';
+        //     document.body.appendChild(colDiv); 
+        // }
 
-for(divCounter = 0; divcounter <= 16; divCounter++ ){ 
-    let gridDiv = document.createElement('div');
-    resultsDiv.setAttribute('class', 'grid-container')
-    document.body.appendChild(gridDiv); 
+        //querySelector('grid-container')
+
+        for (i = 0; i < 16; i++) {
+            for (j = 0; j < 16; j++) {
+            const divContainer = document.querySelector('.grid-container');
+              const row = document.createElement("div");
+              divContainer.appendChild(row);
+              row.textContent = `a`
+          //  }
+          }
+    }
 }
+
+createDivs();
