ec9debbeaa451716e911a3f0d57f358d5dd5d7c5
devin-sachs <75508790+devin-sachs@users.noreply.github.com>
2024-03-12 13:33:30 -0700

    Remove old commented out code for testing in js file to work out grid makin

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
 //Create 16x16 pattern of divs
 function createDivs(){ 
-    //divCounter = 0;
-    //for(divCounterY = 0; divCounterY <= 16; divCounterY++ ){ 
-        // const rowDiv = document.createElement('div');
-        // rowDiv.setAttribute('class', 'grid-container')
-        // rowDiv.textContent = 'a';
-        // document.body.appendChild(rowDiv); 
-        
-        // for(divCounterX =0; divCounterX <=16; divCounterX++){ 
-        //     const colDiv = document.createElement('div');
-        //     colDiv.setAttribute('class', 'grid-container')
-        //     colDiv.textContent = 'b';
-        //     document.body.appendChild(colDiv); 
-        // }
-
-        //querySelector('grid-container')
-
         for (rowCount = 0; rowCount < 16; rowCount++) {
             for (colCount = 0; colCount < 16; colCount++) {
             const divContainer = document.querySelector('.grid-container');
