c3ec08917cb131c7b78133c209376a53f9f89c78
devin-sachs <75508790+devin-sachs@users.noreply.github.com>
2024-03-13 07:46:02 -0700

    Add new class and style for mouseenter event into grid Added logic inside JS to loop through all target elements to apply event listener for all grid-points. Hover mouse over grid will now change background color of grid-point.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+
+
 //Create 16x16 pattern of divs
+
 function createDivs(){ 
         for (rowCount = 0; rowCount < 16; rowCount++) {
             for (colCount = 0; colCount < 16; colCount++) {
@@ -7,9 +10,43 @@ function createDivs(){
               gridPoint.setAttribute('class','grid-point')
               divContainer.appendChild(gridPoint);
               gridPoint.textContent = ` `
-          //  }
+           }
           }
     }
-}
+function gridHighlight(){
+        // testing code
+         // let mouseTargets = document.querySelectorAll("grid-point"); 
+    
+        // mouseTargets.forEach(el => el.addEventListener('mouseenter', event => {
+        //     mouseTargets.classList.add('mouse-on');
+        // }))
+    
+        // // mouseTargets.addEventListener('mouseleave', (e) => {
+        // //     mouseTarget.classList.remove('mouse-on');
+        // // })
+    
+    
+    
+            // if (etchElements[gridCount].classList.contains('mouse-on')) {
+            //     etchElements[gridCount].addEventListener('mouseleave', (e) => {
+            //     etchElements[gridCount].classList.remove('mouse-on');
+            //     })
+            // }
+    
+            // etchElements[gridCount].addEventListener('mouseleave', (e) => {
+            //     etchElements[gridCount].classList.remove('mouse-on');
+            // })
+    
+    
+    
+        let etchElements = document.getElementsByClassName('grid-point');
+    
+        for (let gridCount = 0; gridCount < etchElements.length; gridCount++) {
+            etchElements[gridCount].addEventListener('mouseenter', (e) => {
+                etchElements[gridCount].classList.add('mouse-on');
+            })
+    }
+    }    
 
 createDivs();
+gridHighlight();
