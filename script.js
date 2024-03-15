32ed3861fe6b1f8b0db0038899fb6e48d32a97c7
devin-sachs <75508790+devin-sachs@users.noreply.github.com>
2024-03-15 16:05:47 -0700

    Add JS logic to properly remove grid from system when clicking the custom grid button.
    Add basic logic to create new custom grid. Needs further refinement to work properly.

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 
 //Create 16x16 pattern of divs
 
-function createDivs(){ 
-        for (rowCount = 0; rowCount < 16; rowCount++) {
-            for (colCount = 0; colCount < 16; colCount++) {
+function createDivs(squareCount){ 
+        for (rowCount = 0; rowCount < squareCount; rowCount++) {
+            for (colCount = 0; colCount < squareCount; colCount++) {
             const divContainer = document.querySelector('.grid-container');
               const gridPoint = document.createElement("div");
               gridPoint.setAttribute('class','grid-point')
@@ -47,9 +47,40 @@ function gridHighlight(){
             })
     }
     }
-    
-function newGrid(){
+
+
+function removeGrid() {
+    let removeSquare = document.getElementsByClassName('grid-point');
+
+    while(removeSquare[0]) { 
+    let removeSquare = document.getElementsByClassName('grid-point');
+        removeSquare[0].parentNode.removeChild(removeSquare[0]);
+    }
 }
 
-createDivs();
+function newGrid(){
+
+    const customGrid = document.getElementById('custom-grid');
+    customGrid.addEventListener('click', (event) => {
+        let squareCount = Number(window.prompt(`Enter the Number of squares per side you'd like`, ""))
+        removeGrid()
+        createDivs(squareCount);
+
+        // need to adjust CSS to accomodate new grid and confirm removing the grid works
+    //Need to do some math to adjust square size to fit pixels of container and set max input possible to 100 or less 
+    })
+
+    //To Do:
+
+    // Fix code block to clear grid 
+    // Adjust grid creation function to modify css style for .data-grid to adjust width and height:(calc) 
+    // 100/number of squares per row - 1px ish
+
+}
+
+
+
+
+createDivs(16);
 gridHighlight();
+newGrid();
